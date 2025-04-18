022c4fd1ad7c89614fdcf4a4a156b3d46fb7fa19
JOSHUA EFRAIN CUEVA CAMPOVERDE joshua.cueva@ucuenca.edu.ec <joshua.cueva@ucuenca.edu.ec>
2025-04-18 09:38:32 -0500

    Server is running but does nothing

---
+++ app/main.py
@@ -0,0 +1,35 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="XGBoost with FastAPI",
+    description="API para entrenar y predecir con modelos XGBoost y ajuste bayesiano.",
+    version="1.0.0",
+)
+
+origins = [
+    "http://localhost:5173",  # local url
+    "https://xgboostfrontend.vercel.app",  # frontend url
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router(
+#     neural_network.router, prefix="/neural-network", tags=["Red Neuronal"]
+# )
+
+
+@app.get("/", tags=["root"])
+def root():
+    return {"message": "Bienvenido a la API de BNN con FastAPI"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
